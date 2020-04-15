be9924145d4943b04fce95c77991bcb15ba2bedb
Tom Schrimshaw <tschrimshaw@gmail.com>
2020-04-15 14:52:55 +0100

    adds workable outpu of Js files

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,11 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
+export {
+    checkForName,
+    handleSubmit
+}
+
 console.log(checkForName);
 
-alert("I EXIST")
 console.log("CHANGE!!");
