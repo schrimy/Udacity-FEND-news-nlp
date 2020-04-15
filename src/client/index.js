36e32bd3d5002979c7084cb8d727cc545b7e2f06
Tom Schrimshaw <tschrimshaw@gmail.com>
2020-04-15 16:05:19 +0100

    loads scss inline as css

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,12 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
+//import scss files
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+
 export {
     checkForName,
     handleSubmit
